79197bfc60204f9ad06c257f21327c7aa604e9d7
Kanishk <kanishkpathak21@gamil.com>
2023-08-05 22:03:40 +0530

    inittial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from './components/navbar/NavBar'
+import Form from './components/Form/Form'
+
+const App = () => {
+  return (
+    <><NavBar/>
+    <Form/></>
+  )
+}
+
+export default App
