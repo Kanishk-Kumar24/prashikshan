30fa3b4df82daba9c4430f7a5060f965bc471f49
Kanishk <kanishkpathak21@gamil.com>
2023-08-07 14:52:45 +0530

    basic routing done

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import NavBar from './components/NavBar'
-import Form from './components/Form'
-import Status from './components/Status'
+import React from "react";
+import { Routes, Route, BrowserRouter, RouteProps } from "react-router-dom";
+
+import Form from "./components/Form";
+import Status from "./components/Status";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <><NavBar/>
-    <Form/>
-    
-    
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Form" element={<Form />} />
+          <Route path="/Status" element={<Status />} />
+        </Routes>
+      </BrowserRouter>
     </>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
